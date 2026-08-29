285a45bd1f075c480a50d656a7cf9f276f9dd696
R2bEEaton <R2bEEaton@users.noreply.github.com>
2026-08-29 09:41:43 -0400

    fix: remove unsafe inventory constructor mixins

--- fabric/src/client/java/cc/spea/betterinventory/client/mixin/CreativeModeInventoryScreenMixin.java
+++ fabric/src/client/java/cc/spea/betterinventory/client/mixin/CreativeModeInventoryScreenMixin.java
@@ -4,18 +4,11 @@ import net.minecraft.client.gui.screens.inventory.CreativeModeInventoryScreen;
 import net.minecraft.world.item.CreativeModeTab;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyArgs;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 @Mixin(CreativeModeInventoryScreen.class)
 abstract class CreativeModeInventoryScreenMixin {
-	@ModifyConstant(method = "<init>", constant = @Constant(intValue = 136))
-	private int betterinventory$extendCreativeInventoryHeight(int originalHeight) {
-		return 154;
-	}
-
 	@ModifyArgs(
 		method = "selectTab",
 		at = @At(
